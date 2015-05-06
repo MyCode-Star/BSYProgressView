d667325e03ab875830ed45e80334473b1ed97ff1
baishiyun <baishiyun@163.com>
2015-05-06 19:59:37 +0800

    BSYProgressView
    BSYProgressView

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSYProgressView
+//
+//  Created by mac on 15/5/6.
+//  Copyright (c) 2015年 BSY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BSYProgressView
+//
+//  Created by mac on 15/5/6.
+//  Copyright (c) 2015年 BSY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
